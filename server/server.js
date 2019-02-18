5457f76a5d7c524e9a61934ab8fb6828fbc33ac0
Caleb Asokar <psyca1@nottingham.ac.uk>
2019-02-18 01:28:10 +0000

    added comments to server.js file

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
+//Require the express package.
 const express = require('express');
+
+//Create an instance called 'app'.
 const app = express();
 
-
+//Call the root URL '/' and display some text to visually confirm succesful webpage connection.
 app.get('/', (req,res)=> {
  res.send('Test');
 });
 
 
 
-
+//Display on the console what port the server is running on.
 const server = app.listen(7000, ()=> {
  console.log(`Server successfully running on Port: ${server.address().port}`);
 });
