ea0ff0b1c0dc427fe7406c5bb23c8d5c9d709d98
Caleb Asokar <psyca1@nottingham.ac.uk>
2019-02-21 08:55:44 +0000

    updated file structure to add static folder

---
+++ static/js/Rating.js
@@ -0,0 +1,32 @@
+const ratings = {
+  article1: 4.7,
+  article2: 3.4,
+  article3: 2.3,
+  article4: 3.6,
+  article5: 4.1
+}
+
+const starsTotal = 5;
+
+const articleSelector = document.getElementById('articleSelect');
+const ratingSelector = document.getElementById('ratingSelect');
+
+let article;
+
+articleSelector.addEventListener('change', (e) => {
+  product = e.target.value;
+  var x = document.getElementById("ratingSelect");
+  x.disabled=false;
+  ratingSelector.value = ratings[article];
+});
+
+ratingSelector.addEventListener('blur', (e) => {
+  const rating = e.target.value;
+
+  if (rating > 5) {
+    alert('Please provide a valid rating between 1-5.');
+    return;
+  }
+
+  ratings[article] = rating; 
+});
