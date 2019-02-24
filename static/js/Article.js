12b6f1b5d8cbb8785a713c2721b07aee94fe1511
Caleb Asokar <psyca1@nottingham.ac.uk>
2019-02-24 11:18:14 +0000

    added styling for article 2

--- static/js/Article.js
+++ static/js/Article.js
@@ -74,10 +74,10 @@ function displayArticle2(article2) {
       <div class="col-sm-6">
         <br>
         <p>${parsedArticle2.body[2].model.text}</p> <br> 
-        <p>${parsedArticle2.body[7].model.text}</p> <br style="line-height:5;">
+        <p>${parsedArticle2.body[5].model.text}</p> <br style="line-height:5;">
         <img class="article-photo" src="${parsedArticle2.body[4].model.url}"> <hr>
         <p><strong>${parsedArticle2.body[4].model.altText}</strong></p>
-        <p>${parsedArticle2.body[5].model.text}</p>
+        <p>${parsedArticle2.body[7].model.text}</p>
         
       </div>
     </div>
@@ -87,14 +87,38 @@ function displayArticle2(article2) {
   `;
 }
 
-function displayArticle3() {
+function displayArticle3(article3) {
+  var parsedArticle3 = JSON.parse(article3);
+  document.getElementById("showArticle").innerHTML = `
+    <h1 class="article-title">${parsedArticle3.title}</h1><br>
+    <h2 class="article-header">${parsedArticle3.body[0].model.text}</h2><br>
+    <div class="row">
+      <div class="col-sm-6"
+        <p>${parsedArticle3.body[1].model.text}</p>
+        <hr>
+        <p>${parsedArticle3.body[2].model.text}</p>
+        <hr>
+        <p>${parsedArticle3.body[3].model.text}</p>
+        <hr>
+        <p>${parsedArticle3.body[4].model.text}</p>
+      </div>
+      <div class="col-sm-6"
+        <p>${parsedArticle3.body[5].model.text}</p>
+        <hr>
+        <p>${parsedArticle3.body[6].model.text}</p>
+        <hr>
+        <p>${parsedArticle3.body[7].model.text}</p>
+        <hr>
+        <p>${parsedArticle3.body[8].model.text}</p>
+      </div>
+    </div>
+  `;
+}
+
+function displayArticle4(article4) {
   document.getElementById("showArticle").innerHTML = ``;
 }
 
-function displayArticle4() {
-  document.getElementById("showArticle").innerHTML = ``;
-}
-
-function displayArticle5() {
+function displayArticle5(article5) {
   document.getElementById("showArticle").innerHTML = ``;
 }
