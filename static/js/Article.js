6b096621218cded659f2b25678ebc164b78f98d3
Caleb Asokar <psyca1@nottingham.ac.uk>
2019-02-24 08:50:34 +0000

    updated linkage between pages

--- static/js/Article.js
+++ static/js/Article.js
@@ -1,5 +1,5 @@
-function loadArticle1() {
-  $.get("../articles/article-1.json").then(function(article1){
+function loadArticle() {
+  $.get("../articles/article-" + pageNumber + ".json").then(function(article){
     var obj = JSON.stringify(article1);
     displayArticle1(obj);
     console.log(obj);
@@ -7,9 +7,6 @@ function loadArticle1() {
   }).then(function(article2){
     console.log(article2);
   });
-  /*document.getElementById("showArticle").innerHTML = `
-    <h1 class="app-title"> </h1>
-  `;*/
 }
 
 function displayArticle1(article1){
@@ -17,7 +14,9 @@ function displayArticle1(article1){
   document.getElementById("showArticle").innerHTML = `
     <h1 class="app-title">${article1.title}</h1>
     ${article1.body.map(function(article){
-      return `${article.type}
+      return `
+        <div class="article">
+        </div>
       `
   })}
   `;
@@ -37,4 +36,20 @@ function displayArticle4() {
 
 function displayArticle5() {
   document.getElementById("showArticle").innerHTML = ``;
+}
+
+function displayHeading(header) {
+  
+}
+
+function displayParagraph(paragraph) {
+  
+}
+
+function displayImage(image) {
+  
+}
+
+function displayList(list) {
+  
 }
--- static/js/Rating.js
+++ static/js/Rating.js
@@ -9,10 +9,13 @@ const ratings = {
 const starsTotal = 5;
 
 let article;
+var pageNumber = 1;
 
 const articleSelector = document.getElementById('articleSelect');
 const ratingSelector = document.getElementById('ratingSelect');
-const loadArticle = document.getElementById('loadArticleButton');
+const loadArticleButton = document.getElementById('loadArticleButton');
+const previousButton = document.getElementById('previousButton');
+const nextButton = document.getElementById('nextButton');
 
 
 articleSelector.addEventListener('change', (e) => {
@@ -33,32 +36,51 @@ ratingSelector.addEventListener('blur', (e) => {
 });
 
 function disableButton(buttonAction){
-  if(buttonAction.id=='loadArticleButton'){
+  if(buttonAction.id==loadArticleButton){
      buttonAction.disabled=true;
+  }
+  checkPageButton(2);
+}
+
+/*function nextPageButton() {
+  pageNumber++;
+  if (pageNumber!=5){
+    nextButton.disabled=false;
+  } else {
+    nextButton.disabled=true;
   }
 }
 
-/*window.onload = function(){
-  $.get("../articles/article-1.json").then(function(article1){
-    console.log(article1);
-    return $.get("../articles/article-2.json");
-  }).then(function(article2){
-    console.log(article2);
-    return $.get("../articles/article-3.json");
-  }).then(function(article3){
-    console.log(article3);
-    return $.get("../articles/article-4.json");
-  }).then(function(article4){
-    console.log(article4);
-    return $.get("../articles/article-5.json");
-  }).then(function(article5){
-    console.log(article5);
-  });
+function previousPageButton() {
+  pageNumber--;
+  if (pageNumber!=1){
+    previousButton.disabled=false;
+  } else {
+    previousButton.disabled=true;
+  }
 }*/
 
-
-function displayArticle1() {
-  document.getElementById("showArticle").innerHTML = `Hello, ${1} article is being displayed.`;
+function checkPageButton(changePage) {
+  if(changePage==1){
+    pageNumber++;
+  } else if (changePage==0) {
+    pageNumber--;
+  } else {
+    console.log(pageNumber);
+  }
+  
+  if (pageNumber==5){
+    nextButton.disabled=true;
+    previousButton.disabled=false;
+  } else if (pageNumber==1){
+    nextButton.disabled=false;
+    previousButton.disabled=true;
+  } else {
+    nextButton.disabled=false;
+    previousButton.disabled=false;
+  }
+  
+  loadArticle();
 }
 
 function submitRating(){
